498592b5595ce111903143866a005b7afa5403d9
vicks <pedropuzzi@gmail.com>
2014-04-13 19:24:54 -0300

    atualizando o codigo do merge

--- trabalho01_mips/mergeSort.asm
+++ trabalho01_mips/mergeSort.asm
@@ -3,13 +3,27 @@
 
 main:
 
-	
 	jal bubble
+
+	la $a0, list
+	lw $a1, size
 	
+	jal print_function	
+
+	li $v0,4
+	la $a0,pulalinha
+	syscall
+	
+	li $v0,4
+	la $a0,msg
+	syscall
+
 	li $v0,10
 	syscall
 
-	bubble:
+	endMain:
+
+bubble:
 
 	addi $sp, $sp, -24
 	sw $s0,0($sp)
@@ -19,10 +33,8 @@ main:
 	sw $t2,16($sp)
 	sw $t3,20($sp)
 
-
 	lw $t3,size
-	la $t1,list	  #passa pra t1 o endereço de lista
-		
+	la $t1,list	  #passa pra t1 o endereço de lista	
 
 	lw $s3,size
 	add $s3,$s3,-1
@@ -43,11 +55,11 @@ main:
 			sw 	$t8,list($s1)
 			sw  $t7,list + 4($s1)
 
-		no_swap:
+	no_swap:
 
-			addi $s1,$s1,4
-			addi $s0,$s0,1
-			j inner
+		addi $s1,$s1,4
+		addi $s0,$s0,1
+		j inner
 
 		inner_end:
 
@@ -56,37 +68,34 @@ main:
 
 	outer_end:
 
-		lw	$s0, 0($sp)		# restore $s0 from the stack
-		lw	$s1, 4($sp)		# restore $s1 from the stack
+		lw	$s0, 0($sp)	   # restore $s0 from the stack
+		lw	$s1, 4($sp)	   # restore $s1 from the stack
 		lw	$s3, 8($sp)    # restore $s3 from the stack
     	lw  $t1, 12($sp)
 		lw  $t2, 16($sp)
 		lw  $t3, 20($sp)
-		addi $sp, $sp, 24		# restore stack pointer
+		addi $sp, $sp, 24  # restore stack pointer
 
 
-	end_bubble:
+	endbubble:
 
+		jr	$ra
 	 
 	
-
-	print_function:
+print_function:
 	
-		addi $sp, $sp, -12
-		sw 	 $t1, 0($sp)
-		sw   $t2, 4($sp)
-		sw   $t3, 8($sp)
+	addi $sp, $sp, -12
+	sw 	 $t1, 0($sp)
+	sw   $t2, 4($sp)
+	sw   $t3, 8($sp)
 
-		la $t1,list	  #passa pra t1 o endereço de lista
-		li $t2,0      #loop counter	
-		lw $t3,size
+	move $t1, $a0	  #passa pra t1 o endereço de lista
+	move $t3, $a1    #passa pra t1 o argumento com o tamanho da lista
+	li $t2,0      #loop counter	
 	
 
 	print_loop:
-
-		
-		
-
+	
 		beq $t2,$t3,print_loop_end
 		lw $a0,($t1)
 		li $v0,1
@@ -109,19 +118,44 @@ main:
 		lw  $t3, 8($sp)
 		addi $sp, $sp, 12		# restore stack pointer
 
-
-	li $v0,4
-	la $a0,pulalinha
-	syscall
-
-	
-	li $v0,4
-	la $a0,msg
-	syscall
-
-	jr	$ra
+		jr $ra
 
 	.data
+
+
+merge:
+
+	
+	#$t0 é o endereço para o começo do vetor
+	#$t1 é o tamanho do vetor 
+
+	addi $sp,$sp,-32
+	sw $t0,	0($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+	sw $t4, 16($sp)
+	sw $t5, 20($sp)
+	sw $t6, 24($sp)
+	sw $t7, 28($sp)
+
+
+
+
+
+endmerge:
+
+
+
+
+
+
+
+
+
+
+
+
 
 msg:    .asciiz   "Teste\n"
 size:	.word	  10
